f1368a08912d0d747acf291743f1c925a3bc761c
David Galichet <david.galichet@gmail.com>
2011-06-21 08:13:16 +0200

    add idea plugin

--- xke-akka-scala/project/plugins/Plugins.scala
+++ xke-akka-scala/project/plugins/Plugins.scala
@@ -3,4 +3,7 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val akkaRepo   = "Akka Repo" at "http://akka.io/repository"
   val akkaPlugin = "se.scalablesolutions.akka" % "akka-sbt-plugin" % "1.1"
+
+  val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+  val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.4.0"
 }
