623b1f2c937c14040cda0abf540db2cbd9b0994f
Julien Buret <julien.buret@gmail.com>
2011-07-06 17:10:15 +0200

    update pres

---
+++ xke-akka-java/src/test/java/fr/xebia/xkeakka/manufacturing/TestStorageManager.java
@@ -0,0 +1,11 @@
+package fr.xebia.xkeakka.manufacturing;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jburet
+ * Date: 06/07/11
+ * Time: 15:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestStorageManager {
+}
